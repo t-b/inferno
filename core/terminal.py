b40bdc9affa35037dd36d2775b3178e469a96261
Tom Gillespie <tgbugs@gmail.com>
2014-05-13 23:25:43 -0700

    adding focuse events to things captured since cmd.exe doesnt register mouse events

--- core/terminal.py
+++ core/terminal.py
@@ -72,7 +72,13 @@ def GetTerminalEvent():
                 # params: handler, buffer, length, eventsnum
                 ctypes.windll.kernel32.ReadConsoleInputW(ch, ctypes.byref(inbuf), 2, ctypes.byref(eventread))
                 EVENT_TYPE = EVENTS[inbuf[0].eventType]
-                if EVENT_TYPE != 'KEY_EVENT' and EVENT_TYPE != 'MOUSE_EVENT':
+                if EVENT_TYPE != 'KEY_EVENT' 
+                    print(EVENTS[inbuf[0].eventType])
+                    pass
+                elif EVENT_TYPE != 'MOUSE_EVENT':
+                    print(EVENTS[inbuf[0].eventType])
+                    pass
+                elif EVENT_TYPE != 'FOCUS_EVENT':
                     print(EVENTS[inbuf[0].eventType])
                     pass
                 else:
